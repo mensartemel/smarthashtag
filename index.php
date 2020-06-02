161b0728605a5eae6cfcb5638553b4bc858458fd
mensartemel <53830523+mensartemel@users.noreply.github.com>
2020-06-02 07:01:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,28 @@
 <script src="script.js"></script>
 </head>
 <body>
+  <div class="container-fluid">
+		<div class="row">
+			<div class="col-2">
+				<div class="banner">
+					<img class="profile-img" src="pic.jpg" alt="Profile Picture";>
+					<a>User</a>
+				</div>
+				<div class="vertical-menu">
+					<a href="index.php" class="menu-btn"><img class="menu-icon" src="img/icon/dashboard.png";>Dashboard</a>
+					<a href="index.php?page=apps" class="menu-btn"><img class="menu-icon" src="img/icon/dashboard.png";>My Applications</a>
+					<a href="index.php?page=service" class="menu-btn"><img class="menu-icon" src="img/icon/stats.png";>Services</a>
+					<a href="index.php?page=search" class="menu-btn"><img class="menu-icon" src="img/icon/stats.png";>Search Example</a>
+					<a href="index.php?page=settings" class="menu-btn"><img class="menu-icon" src="img/icon/settings.png";>Settings</a>
+					<a href="logout.php" class="menu-btn bottom active"><img class="menu-icon" src="img/icon/logout.png";>Log Out</a>
+				</div>
+			</div>
+			<div class="col-10">
+				
+    </div>
+			</div>
+		</div>
+
+	</div>
 </body>
 </html>
