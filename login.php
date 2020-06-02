58c4aa7c7101b97c4e3ba08ad122f91428fd6540
mensartemel <53830523+mensartemel@users.noreply.github.com>
2020-06-02 06:53:26 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login via Twitter</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="style.css">
+<script src="script.js"></script>
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <a href="twitter_login.php"><img src="img/twitter_button.png" alt="Login via Twitter" width="100%";></a>
+  </div>
+</div>
+</body>
+</html>
