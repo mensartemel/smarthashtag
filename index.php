f23d1eea2e88304fdb6acfa854c56c205119904a
mensartemel <ensarullah.temel@gmail.com>
2020-06-02 07:10:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once './config.php';
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+if (!isset($_SESSION["loggedin"])) {
   header("location:login.php");
 }
 else {
