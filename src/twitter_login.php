0d4f6d584ea8305ae6ad8af27e5caa35098ee9d4
mensartemel <ensarullah.temel@gmail.com>
2020-06-04 20:00:36 +0300

    12

--- src/twitter_login.php
+++ src/twitter_login.php
@@ -78,6 +78,7 @@ if ($success) {
         $_SESSION["e_msg"] = "";
       }
     }
+    header("location: ../consumer.php");
   } else {
     $sql = "SELECT id from users where twitter_id = :id";
     try {
