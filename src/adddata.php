5b2db5f7fe1287ed53a1b7cd45f3bee0a6b46798
mensartemel <ensarullah.temel@gmail.com>
2020-06-05 00:53:50 +0300

    asd

--- src/adddata.php
+++ src/adddata.php
@@ -4,7 +4,7 @@ if($number > 1)
 {
 	for($i=0; $i<$number; $i++)
 	{
-		if(trim($_POST["name"][$i] != ''))
+		if(trim($_POST["keyword"][$i] != ''))
 		{
 			// $sql = "INSERT INTO tbl_name(name) VALUES('".mysqli_real_escape_string($connect, $_POST["name"][$i])."')";
 			echo $_POST["keyword"][$i]." ". $_POST["profilesearch"][$i] ."</br>";
