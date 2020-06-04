ed50002915fbe783360065f8ce3294bac914ef1a
mensartemel <ensarullah.temel@gmail.com>
2020-06-04 03:05:49 +0300

    123

---
+++ addapp.php
@@ -0,0 +1,54 @@
+<?php
+function generateKey($length = 26) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $twitter_id = $_SESSION["twitter_id"];
+  $appname = $_POST["appname"];
+  $desc = $_POST["desc"];
+  $key = generateKey();
+
+  $sql = "SELECT id from users where twitter_id = :id";
+  $stmt = $DB->prepare($sql);
+  $stmt->bindValue(":id", $twitter_id);
+  $stmt->execute();
+  $result = $stmt->fetch();
+  $userid = $result["id"];
+
+  $sql = "INSERT INTO `applications` (`appname`, `description`, `appkey`, `userid`) VALUES " . "( :appname, :description, :appkey, :userid)";
+  $stmt = $DB->prepare($sql);
+  $stmt->bindValue(":appname", $appname);
+  $stmt->bindValue(":description", $desc);
+  $stmt->bindValue(":appkey", $key);
+  $stmt->bindValue(":userid", $userid);
+  $stmt->execute();
+  $_SESSION["e_msg"] = "Application created successfully!";
+  header("location:index.php?page=apps");
+}
+?>
+
+<form method="post" action="">
+  <div>
+    <label for="appname">Application Name:</label>
+    <div >
+      <input type="text" placeholder="New Application" name="appname">
+    </div>
+  </div>
+  <div>
+    <label for="desc">Description:</label>
+    <div>
+      <input type="text" placeholder="Description" name="desc">
+    </div>
+  </div>
+  <div>
+    <div>
+      <button type="submit">Add</button>
+    </div>
+  </div>
+</form>
--- index.php
+++ index.php
@@ -7,6 +7,10 @@
   $profile_pic = $_SESSION["picture"];
   $name = $_SESSION["name"];
 ?>
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of b70b9e1... asd
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,7 +43,11 @@
             if(isset($_GET['page']))
             {
               if ($_GET['page'] == "apps") {
+<<<<<<< HEAD
                 echo "<div class='title'><a>Applications</a></div><div class='titleside'><button id='myBtn2' onclick='openaddapp()' name='addapp' class='button'>Add Application</button></div>";
+=======
+                echo "<div class='title'><a>Applications</a></div><div class='titleside'><a class='button' href='index.php?page=addapp'>Add Application</a></div>";
+>>>>>>> parent of b70b9e1... asd
               }
               elseif ($_GET['page'] == "addapp") {
                 echo "<div class='title'><a>Add New Application</a></div>";
@@ -115,7 +123,10 @@
     </div>
   </div>
 </div>
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of b70b9e1... asd
 </body>
 </html>
 <script>
