6f03a3b4297d8466b27a92c58473ad0373eb3559
mensartemel <ensarullah.temel@gmail.com>
2020-06-05 03:20:02 +0300

    asd

--- src/adddata.php
+++ src/adddata.php
@@ -14,36 +14,19 @@ if($number >= 1)
   $stmt->bindValue(":consumerid", $consumerid);
   $stmt->execute();
   $smarthashtagid = $DB->lastInsertId();
-  echo "New hashtag ".$smarthashtagid;
-	for($i=0; $i<$number; $i++)
-	{
-		if(trim($_POST["keyword"][$i] != ''))
-		{
-      $sql = "SELECT * FROM keywords WHERE keyword ="." :keyword";
-      $stmt = $DB->prepare($sql);
-      $stmt->bindValue(":keyword", $_POST["keyword"][$i]);
-      $stmt->execute();
-      $result = $stmt->fetch();
-      $keywordid = $result["id"];
-      if ($result > 0) {
-        $sql = "INSERT INTO sh_kw (shid, kwid) VALUES"." (:shid, :kwid)";
-        $stmt = $DB->prepare($sql);
-        $stmt->bindValue(":shid", $smarthashtagid);
-        $stmt->bindValue(":kwid", $keywordid);
-        $stmt->execute();
-        $result = $stmt->rowCount();
-        if ($result > 0) {
-          echo "New hashtag, new keyword inserted";
-        }
-        echo "New hashtag, error while keyword inserted";
-      }
-      else {
-        $sql = "INSERT INTO keywords (keyword) VALUES "."(:keyword)";
+  if ($smarthashtagid > 0) {
+    echo "New hashtag ".$smarthashtagid;
+    for($i=0; $i<$number; $i++)
+    {
+      if(trim($_POST["keyword"][$i] != ''))
+      {
+        $sql = "SELECT * FROM keywords WHERE keyword ="." :keyword";
         $stmt = $DB->prepare($sql);
         $stmt->bindValue(":keyword", $_POST["keyword"][$i]);
         $stmt->execute();
-        $keywordid = $DB->lastInsertId();
-        if ($keywordid > 0) {
+        $result = $stmt->fetch();
+        $keywordid = $result["id"];
+        if ($result > 0) {
           $sql = "INSERT INTO sh_kw (shid, kwid) VALUES"." (:shid, :kwid)";
           $stmt = $DB->prepare($sql);
           $stmt->bindValue(":shid", $smarthashtagid);
@@ -51,13 +34,33 @@ if($number >= 1)
           $stmt->execute();
           $result = $stmt->rowCount();
           if ($result > 0) {
-            echo "New hashtag, existing keyword inserted";
+            echo "New hashtag, new keyword inserted";
           }
-          echo "New hashtag, error while existing keyword inserted";
+          echo "New hashtag, error while keyword inserted";
+        }
+        else {
+          $sql = "INSERT INTO keywords (keyword) VALUES "."(:keyword)";
+          $stmt = $DB->prepare($sql);
+          $stmt->bindValue(":keyword", $_POST["keyword"][$i]);
+          $stmt->execute();
+          $keywordid = $DB->lastInsertId();
+          if ($keywordid > 0) {
+            $sql = "INSERT INTO sh_kw (shid, kwid) VALUES"." (:shid, :kwid)";
+            $stmt = $DB->prepare($sql);
+            $stmt->bindValue(":shid", $smarthashtagid);
+            $stmt->bindValue(":kwid", $keywordid);
+            $stmt->execute();
+            $result = $stmt->rowCount();
+            if ($result > 0) {
+              echo "New hashtag, existing keyword inserted";
+            }
+            echo "New hashtag, error while existing keyword inserted";
+          }
         }
       }
-		}
-	}
+    }
+  }
+  echo "Error while creating hashtag";
 }
 else
 {
