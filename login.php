6734aa11dca61eaeaa1bad7320e1503f932814df
mensartemel <ensarullah.temel@gmail.com>
2020-06-06 02:48:32 +0300

    asd

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php
+$betaErr = $beta = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $betaErr = $beta = "";
   if (empty($_POST["betacode"])) {
     $betaErr = "Betocode is required";
   } else {
-    $beta = test_input($_POST["name"]);
+    $beta = test_input($_POST["betacode"]);
     // check if name only contains letters and whitespace
-    if ($name != "estu2020") {
+    if ($beta != "estu2020") {
       $betaErr = "The code you entered does not match";
     } else {
-      header("location:index.php?page=apps");
-    }
+		header("location:index.php?page=apps");
+	}
   }
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
 ?>
 <!DOCTYPE html>
@@ -26,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="login-page">
   <div class="form">
     <form method="post" action="">
-      <label for="appname">Beta Code:</label><span class="error"><?php echo $betaErr;?></span>
+      <label for="appname">Beta Code:</label></br><span class="error"><?php echo $betaErr;?></span>
       <input type="text" placeholder="New Application" name="betacode">
       <button class="appform" type="submit">Login via Twitter</button>
     </form>
