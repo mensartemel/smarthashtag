8b7bca5df4dd2fdd0fe85c01d13fc91cd2346b4c
mensartemel <ensarullah.temel@gmail.com>
2020-06-06 10:46:41 +0300

    asd

--- src/adddata.php
+++ src/adddata.php
@@ -137,7 +137,7 @@ if($number >= 1)
       }
     }
     if ($errorCount > 0) {
-      echo $errorCount."Hata";
+      echo $errorCount." Error";
     } else {
       $sql = "SELECT * FROM consumer_results WHERE shid ="." :shid "." ORDER BY RAND() LIMIT 10";
       $stmt = $DB->prepare($sql);
