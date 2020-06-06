4aa12f03ce1597f8553386047bc716f874be7946
mensartemel <ensarullah.temel@gmail.com>
2020-06-06 05:21:44 +0300

    asd

--- src/adddata.php
+++ src/adddata.php
@@ -79,7 +79,7 @@ if($number >= 1)
 
     for($i=0; $i<$number; $i++) {
       if(trim($_POST["keyword"][$i] != '')) {
-        $results = $twitter->search(['q' => $_POST["keyword"][$i], 'lang' => $lang, 'result_type' => 'mixed', 'count' => '10']);
+        $results = $twitter->search(['q' => $_POST["keyword"][$i], 'lang' => $lang, 'result_type' => 'popular', 'count' => '10']);
         foreach ($results as $status) {
           $sql = "INSERT INTO consumer_results (screenname, picture, status, created_at, consumerid, keywordid, appid, shid) VALUES "." (:screenname, :picture, :status, NOW(), :consumerid, :keywordid, :appid, :shid)";
           $stmt = $DB->prepare($sql);
