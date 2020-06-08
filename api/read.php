e80fa0de5b3ef1bd40b96019390df96274cd56e8
mensartemel <ensarullah.temel@gmail.com>
2020-06-09 00:00:25 +0300

    asd

--- api/read.php
+++ api/read.php
@@ -1,7 +1,7 @@
 <?php
   // Headers
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
+  //header('Access-Control-Allow-Origin: *');
+  //header('Content-Type: application/json');
 
   include_once '../src/config.php';
 
