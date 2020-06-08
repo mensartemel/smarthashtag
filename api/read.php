b9d45117e77f1d4cc43477a09dac516a821eba87
mensartemel <ensarullah.temel@gmail.com>
2020-06-08 23:53:03 +0300

    asd

--- api/read.php
+++ api/read.php
@@ -27,40 +27,43 @@
   echo $appid." ";
   echo $consumerid." ";
 
-  $sql2 = "SELECT * FROM consumer_results WHERE consumerid = :consumerid AND appid = :appid";
-  $stmt2 = $DB->prepare($sql2);
-  $stmt2->bindValue(":consumerid", $consumerid, PDO::PARAM_INT);
-  $stmt2->bindValue(":appid", $appid, PDO::PARAM_INT);
-  $stmt2->execute();
-  $result = $stmt2;
+  if (($conscount == 1) && ($appcount ==1)) {
+    $sql2 = "SELECT * FROM consumer_results WHERE consumerid = :consumerid AND appid = :appid";
+    $stmt2 = $DB->prepare($sql2);
+    $stmt2->bindValue(":consumerid", $consumerid, PDO::PARAM_INT);
+    $stmt2->bindValue(":appid", $appid, PDO::PARAM_INT);
+    $stmt2->execute();
+    $result = $stmt2;
 
-  $num = $result->rowCount();
-  // Check if any posts
-  if($num > 0) {
-    // Post array
-    $posts_arr = array();
-    // $posts_arr['data'] = array();
+    $num = $result->rowCount();
+    // Check if any posts
+    if($num > 0) {
+      // Post array
+      $posts_arr = array();
+      // $posts_arr['data'] = array();
 
-    while($row = $result->fetch()) {
-      $post_item = array(
-        'resultid' => $row["resultid"],
-        'screenname' => $row["screenname"],
-        'picture' => $row["picture"],
-        'status' => $row["status"],
-        'created_at' => $row["created_at"],
+      while($row = $result->fetch()) {
+        $post_item = array(
+          'resultid' => $row["resultid"],
+          'screenname' => $row["screenname"],
+          'picture' => $row["picture"],
+          'status' => $row["status"],
+          'created_at' => $row["created_at"],
+        );
+
+        // Push to "data"
+        array_push($posts_arr, $post_item);
+        // array_push($posts_arr['data'], $post_item);
+      }
+
+      // Turn to JSON & output
+      echo json_encode($posts_arr);
+
+    } else {
+      // No Posts
+      echo json_encode(
+        array('message' => 'No Posts Found')
       );
-
-      // Push to "data"
-      array_push($posts_arr, $post_item);
-      // array_push($posts_arr['data'], $post_item);
     }
-
-    // Turn to JSON & output
-    echo json_encode($posts_arr);
-
-  } else {
-    // No Posts
-    echo json_encode(
-      array('message' => 'No Posts Found')
-    );
   }
+?>
