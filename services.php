26dc812acdd8c3354f9dc5a9bff791932f74635a
mensartemel <ensarullah.temel@gmail.com>
2020-06-11 19:35:14 +0300

    asd

--- services.php
+++ services.php
@@ -1,10 +1,10 @@
-<div class="col-6">
+<div class="service">
   <img width="100%" src="img/app-details.PNG" alt="App Details"/>
   <p>When you create an app we provide a consumer URL and security key. With consumer URL you can reach your consumer to create samrthashtag.</p>
   <img width="100%" src="img/create-sh.PNG" alt="App Details"/>
   <p>Consumers can enter up to 5 keyword and select language and profile search. Language will bring results with the same language and profile search will bring user profiles that matches with the given keyword.</p>
 </div>
-<div class="col-6">
+<div class="service">
   <img width="100%" src="img/sh-results.PNG" alt="App Details"/>
   <p>When smarthastag created user get 10 random results and rate them.</p>
   <img width="100%" src="img/api-reference.PNG" alt="App Details"/>
