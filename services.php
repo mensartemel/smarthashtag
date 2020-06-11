444365e80a4a56290a3cc45896b473675f35f4d9
mensartemel <ensarullah.temel@gmail.com>
2020-06-11 19:25:18 +0300

    asd

--- services.php
+++ services.php
@@ -1,1 +1,8 @@
-<p>Welcome <?php echo $_SESSION["name"]; ?>.</p>
+<div class="col-6">
+  <img width="100%" src="img/app-details.png" alt="App Details"/>
+  <img width="100%" src="img/create-sh.png" alt="App Details"/>
+</div>
+<div class="col-6">
+  <img width="100%" src="img/sh-results.png" alt="App Details"/>
+  <img width="100%" src="img/api-reference.png" alt="App Details"/>
+</div>
